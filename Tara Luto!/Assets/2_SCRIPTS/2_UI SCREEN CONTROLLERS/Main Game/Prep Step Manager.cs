a0399a08796f4069f40bbc92c105c74f6eec3c5b
solar-ym <168715276+solar-ym@users.noreply.github.com>
2026-07-06 07:52:54 +0800

    main game

---
+++ Tara Luto!/Assets/2_SCRIPTS/2_UI SCREEN CONTROLLERS/Main Game/Prep Step Manager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrepStepManager : MonoBehaviour
+{
+    [Header("ProceedScreen")]
+    [SerializeField] GameObject proceedScreen;
+
+    [Header("Timer")]
+    [SerializeField] Image timerFill;
+    [SerializeField] GameObject timerHand;
+
+    [Header("Completion")]
+    [SerializeField] Image barFill;
+
+    private bool isCounting = false;
+    private float timer = 0f;
+    private float timerMax = 30f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isCounting)
+        {
+            timer += Time.deltaTime;
+            timerFill.fillAmount -= 0.033f * Time.deltaTime;
+            barFill.fillAmount += 0.033f * Time.deltaTime;
+            timerHand.transform.rotation *= Quaternion.Euler(0f, 0f, 12f * Time.deltaTime);
+        }
+
+        if (timer > timerMax)
+        {
+            timer = 0f;
+            timerFill.fillAmount = 1f;
+            barFill.fillAmount = 0f;
+            timerHand.transform.rotation.Set(0f, 0f, 0f, 1f);
+            isCounting = false;
+
+            proceedScreen.SetActive(true);
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    public void StartCounting()
+    {
+        isCounting = true;
+    }
+}
---
+++ Tara Luto!/Assets/2_SCRIPTS/2_UI SCREEN CONTROLLERS/Main Game/Selection Manager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectionManager : MonoBehaviour
+{
+    // ALL CONTENT IS TEMP
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("1_MAIN MENU");
+    }
+}
