fe9f7ecc38b99b0e6cdb89d7864f8131f69183e8
SADIO-M <megan_florence_sadio@dlsu.edu.ph>
2026-08-02 20:46:11 +0800

    Created Level Manager + Can Load Leche Flan Level

---
+++ Tara Luto!/Assets/2_SCRIPTS/1_MANAGERS/Level Manager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private Button beginTapsilogButton;
+    [SerializeField] private Button beginLumpiaButton;
+    [SerializeField] private Button beginAdoboButton;
+    [SerializeField] private Button beginLecheFlanButton;
+
+    private void Awake()
+    {
+        beginTapsilogButton.onClick.AddListener(LoadTapsilogLevel);
+        beginLumpiaButton.onClick.AddListener(LoadLumpiaLevel);
+        beginAdoboButton.onClick.AddListener(LoadAdoboLevel);
+        beginLecheFlanButton.onClick.AddListener(LoadLecheFlanLevel);
+    }
+    private void LoadTapsilogLevel()
+    {
+        //PLACE TAPSILOG LEVEL HERE
+        Debug.Log("Loaded tapsilog level");
+    }
+
+    private void LoadLumpiaLevel()
+    {
+        //PLACE LUMPIA LEVEL HERE
+        Debug.Log("Loaded lumpia level");
+    }
+    private void LoadAdoboLevel()
+    {
+        //PLACE ADOBO LEVEL HERE
+        Debug.Log("Loaded adobo level");
+    }
+
+    private void LoadLecheFlanLevel()
+    {
+        SceneManager.LoadScene("6_LECHE FLAN");
+    }
+
+}
